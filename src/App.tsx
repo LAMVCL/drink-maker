c059fede2acab026cbe2fbfe1e35a3871ffbef68
Luis Martínez <lamvmartinez@outlook.com>
2023-01-29 22:05:51 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import LoadingButton from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import {TextField} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [data, setData] = useState({data: []});
+    const [isLoading, setIsLoading] = useState(false);
+
+    const searchCocktail = async (cocktail : string) => {
+        setIsLoading(true);
+        try{
+            const response = await fetch(`https://api.api-ninjas.com/v1/cocktail?name=${cocktail}`)
+            const newData = await response.json();
+            setData(newData);
+            console.log(data);
+            console.log(cocktail);
+        }catch (e) {
+            console.log(e);
+        }
+    }
+
+    return (
+        <div className="App">
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                        Welcome Tipsy Bartender !
+                    </Typography>
+                    <hr/>
+                    <Typography sx={{ fontSize: 16 }} component="div">
+                        Please search your favorites cocktails here.
+                    </Typography>
+                    <TextField fullWidth id="outlined-basic" size={"small"} label="Cocktail" variant="outlined" />
+                </CardContent>
+                <CardActions>
+                    <LoadingButton variant={"contained"} onClick={() => searchCocktail("vodka")}>
+                        Search!
+                    </LoadingButton>
+                </CardActions>
+            </Card>
+        </div>
+    );
 }
 
 export default App;
